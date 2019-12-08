e97b42e9e5c0b91e74ba83abe51e82c7713eaacc
Mike <michael.valenciav@gmail.com>
2019-12-07 19:50:23 -0500

    tests checking user authentication when signing up.

--- users/tests/test_signup.py
+++ users/tests/test_signup.py
@@ -1,6 +1,7 @@
 """Users signup testcases"""
 import pytest
 
+from django.contrib import auth
 from django.contrib.auth.models import User
 
 
@@ -12,19 +13,29 @@ def test_signup_get_request(client):
 
 @pytest.mark.django_db
 def test_signup_user_creation(client):
-    """On succesful POST request, a User should be created."""
+    """
+    On succesful POST request, a User should be created and the user should
+     be authenticated.
+    """
     assert User.objects.count() == 0
     client.post(
         "", {"username": "eric", "password1": "centipad", "password2": "centipad"}
     )
     assert User.objects.count() == 1
+    user = auth.get_user(client)
+    assert user.is_authenticated
 
 
 @pytest.mark.django_db
 def test_signup_fail(client):
-    """On unsuccesful POST request, a User should not be created."""
+    """
+    On unsuccesful POST request, a User should not be created and the user
+     should not be authenticated.
+    """
     assert User.objects.count() == 0
     client.post(
         "", {"username": "eric", "password1": "centipad", "password2": "2312"}
     )
     assert User.objects.count() == 0
+    user = auth.get_user(client)
+    assert not user.is_authenticated
