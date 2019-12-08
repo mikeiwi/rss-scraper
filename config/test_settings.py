1054bf7270e292220065262f6717985fe7556221
Mike <michael.valenciav@gmail.com>
2019-12-07 19:29:29 -0500

    user creation on signup test.

--- config/test_settings.py
+++ config/test_settings.py
@@ -7,12 +7,11 @@ logging.disable(logging.CRITICAL)
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "test",
-        "USER": "test",
-        "PASSWORD": "test",
-        "HOST": "localhost",
-        "PORT": "",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
--- users/tests/test_signup.py
+++ users/tests/test_signup.py
@@ -1,8 +1,20 @@
 """Users signup testcases"""
 import pytest
 
+from django.contrib.auth.models import User
+
 
 def test_signup_get_request(client):
     """On GET request, a 200 status code is responded."""
     response = client.get("")
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_signup_user_creation(client):
+    """On succesful POST request, a User should be created."""
+    assert User.objects.count() == 0
+    client.post(
+        "", {"username": "eric", "password1": "centipad", "password2": "centipad"}
+    )
+    assert User.objects.count() == 1
