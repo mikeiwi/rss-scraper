210f1058bed78ac7e22655be1164de89531febd6
Mike <michael.valenciav@gmail.com>
2019-12-08 12:44:24 -0500

    login view succesful.

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM python:3
 ENV PYTHONUNBUFFERED 1
 RUN mkdir /code
 WORKDIR /code
+COPY requirements-dev.txt /code/
 COPY requirements.txt /code/
-RUN pip install -r requirements.txt
+RUN pip install -r requirements-dev.txt
 COPY . /code/
--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import redirect
 
 
 def login_view(request):
+    form = AuthenticationForm(data=request.POST)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect("login")
+
     return render(request, "users/signup.html")
 
 
@@ -12,7 +20,6 @@ def signup(request):
     """User registration view."""
     form = UserCreationForm(request.POST)
     if form.is_valid():
-        form = UserCreationForm(request.POST)
         form.save()
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password1")
