3bab83eebf4138900b74eafefcb1555d8b43a0c7
Mike <michael.valenciav@gmail.com>
2019-12-14 18:43:15 -0500

    feed update tests refactored.

--- feeds/tests/tests_feed_update.py
+++ feeds/tests/tests_feed_update.py
@@ -5,46 +5,43 @@ from model_bakery import baker
 from feeds.tasks import update_feed
 
 
-@pytest.mark.django_db
-def test_feedparser_called(mocker, feedparser_data):
-    """Feed updating should call feedparser"""
+@pytest.fixture
+def test_feed():
+    """Creates a fresh baked Feed instance."""
+    return baker.make("Feed", url="https://my-grandma-frilrting-lessons/rss")
+
+
+@pytest.fixture
+def feedparser_mock(mocker, feedparser_data):
+    """Mocks feedparser library and returns some fixture data."""
     m = mocker.patch("feedparser.parse")
     m.return_value = feedparser_data
-    feed = baker.make("Feed", url="https://hannibal-cooking-course.info/rss")
-    update_feed(feed.id)
-    m.assert_called_with("https://hannibal-cooking-course.info/rss")
+    return m
 
 
 @pytest.mark.django_db
-def test_parsed_entries(mocker, feedparser_data):
-    """Parsed info and entries should be inserted in DB."""
-    m = mocker.patch("feedparser.parse")
-    m.return_value = feedparser_data
-    feed = baker.make("Feed")
-    update_feed(feed.id)
-    assert feed.entry_set.count() == 3
+def test_feedparser_called(feedparser_mock, test_feed):
+    """Feed updating should call feedparser and insert entries in DB"""
+    update_feed(test_feed.id)
+    feedparser_mock.assert_called_with(test_feed.url)
+    assert test_feed.entry_set.count() == 3
 
 
 @pytest.mark.django_db
-def test_parsed_entries_update(mocker, feedparser_data):
+def test_parsed_entries_update(feedparser_mock, test_feed, feedparser_data):
     """Parsed info and entries should be inserted in DB, unless they already exist."""
-    m = mocker.patch("feedparser.parse")
-    m.return_value = feedparser_data
-    feed = baker.make("Feed")
-    baker.make("Entry", feed=feed, link=feedparser_data["entries"][0]["link"])
-    update_feed(feed.id)
-    assert feed.entry_set.count() == 3
+    baker.make("Entry", feed=test_feed, link=feedparser_data["entries"][0]["link"])
+    update_feed(test_feed.id)
+    assert test_feed.entry_set.count() == 3
 
 
 @pytest.mark.django_db
-def test_gone_feed(mocker):
+def test_gone_feed(feedparser_mock, test_feed):
     """When a feed is parsed with 410 status, it should be set as gone"""
-    m = mocker.patch("feedparser.parse")
-    m.return_value = {"status": 410}
-    feed = baker.make("Feed")
-    update_feed(feed.id)
-    feed.refresh_from_db()
-    assert feed.gone is True
+    feedparser_mock.return_value = {"status": 410}
+    update_feed(test_feed.id)
+    test_feed.refresh_from_db()
+    assert test_feed.gone is True
 
 
 @pytest.mark.django_db
@@ -56,24 +53,20 @@ def test_gone_feed(mocker):
         {"status": 200, "bozo": 1},
     ],
 )
-def test_parsing_fail(mocker, feedparser_test_data):
+def test_parsing_fail(feedparser_mock, test_feed, feedparser_test_data):
     """
     When parsing fails somehow (failed request or malformed rss), 1 is added to
     a counter for failed_tries.
     """
-    m = mocker.patch("feedparser.parse")
-    m.return_value = feedparser_test_data
-    feed = baker.make("Feed")
-    update_feed(feed.id)
-    feed.refresh_from_db()
-    assert feed.failed_tries == 1
+    feedparser_mock.return_value = feedparser_test_data
+    update_feed(test_feed.id)
+    test_feed.refresh_from_db()
+    assert test_feed.failed_tries == 1
 
 
 @pytest.mark.django_db
-def test_reset_failed_tries(mocker, feedparser_data):
+def test_reset_failed_tries(feedparser_mock, feedparser_data):
     """On succesful parse, `failed_tries` counter should be reset."""
-    m = mocker.patch("feedparser.parse")
-    m.return_value = feedparser_data
     feed = baker.make("Feed", failed_tries=42)
     update_feed(feed.id)
     feed.refresh_from_db()
