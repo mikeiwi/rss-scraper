0291a7e06188ab698eaabec553522bc8291497c3
Mike <michael.valenciav@gmail.com>
2019-12-15 15:28:25 -0500

    migratrion files and some litle teaks.

--- feeds/urls.py
+++ feeds/urls.py
@@ -4,9 +4,9 @@ from .views import FeedListView, FeedEntriesListView, BookmarkListView
 from .views import bookmark_entry, follow_feed
 
 urlpatterns = [
-    path("/list", FeedListView.as_view(), name="user_feed_list"),
-    path("/follow", follow_feed, name="user_feed_follow"),
-    path("/<int:feed_id>", FeedEntriesListView.as_view(), name="feed_entries"),
-    path("/bookmark/<int:entry_id>", bookmark_entry, name="bookmark_entry"),
-    path("/bookmark", BookmarkListView.as_view(), name="bookmark_list"),
+    path("", FeedListView.as_view(), name="user_feed_list"),
+    path("follow", follow_feed, name="user_feed_follow"),
+    path("<int:feed_id>", FeedEntriesListView.as_view(), name="feed_entries"),
+    path("bookmark/<int:entry_id>", bookmark_entry, name="bookmark_entry"),
+    path("bookmark", BookmarkListView.as_view(), name="bookmark_list"),
 ]
--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import login_view, signup
 
 urlpatterns = [
-    path("/signup", signup, name="signup"),
+    path("signup", signup, name="signup"),
     path("", login_view, name="login"),
 ]
